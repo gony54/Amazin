e2a3029bd0e899fadd1128cb4ae0ec73b6682547
Gonzalo Alonso <68425727+gony54@users.noreply.github.com>
2025-10-20 12:15:21 +0200

    Delete Amazin_15_0_0 directory

--- Amazin_15_0_0/src/main/java/com/miw/persistence/DataServicesFactory.java
+++
@@ -1,9 +0,0 @@
-package com.miw.persistence;
-
-import com.miw.persistence.book.BookDataService;
-import com.miw.persistence.vat.VATDataService;
-
-public interface DataServicesFactory {
-	public BookDataService getBookDataService();
-	public VATDataService getVATDataService();
-}
--- Amazin_15_0_0/src/main/java/com/miw/presentation/di/ServletContextAware.java
+++
@@ -1,7 +0,0 @@
-package com.miw.presentation.di;
-
-import jakarta.servlet.ServletContext;
-
-public interface ServletContextAware {
-	public void setServletContext( ServletContext context );
-}
