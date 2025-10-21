d619ebb326a9fb1baa66e097f87b76a895de86c7
Gonzalo Alonso <68425727+gony54@users.noreply.github.com>
2025-10-21 10:27:58 +0200

    Delete Amazin_15_0_0 directory

--- Amazin_15_0_0/src/main/java/com/miw/presentation/commands/Command.java
+++
@@ -1,9 +0,0 @@
-package com.miw.presentation.commands;
-
-// import org.apache.logging.log4j.*;
-
-public interface Command {
-	// Logger logger = LogManager.getLogger(Command.class);
-
-	public abstract void execute();
-}
